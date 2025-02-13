7956c3ace3c2affdcb83317d1fadea9e1c552636
jhkim <jhkim@kyentertainment.kr>
2025-02-13 11:22:14 +0900

    관심종목 추가/삭제 후 종목 정보 요청 시  OpenAPI CommKwRqData 함수를 이용하도록 수정

--- viewmodel/favoriteStockViewModel.py
+++ viewmodel/favoriteStockViewModel.py
@@ -9,6 +9,7 @@ logger = logging.getLogger()
 
 class FavoriteStockViewModel(QObject):
     stockListChanged = pyqtSignal()
+    priceInfoKeys_ = ['시가', '고가', '저가', '현재가', '기준가', '전일대비기호', '전일대비', '등락율', '거래량', '전일거래량대비']
 
     def __init__(self, qmlContext, parent=None):
         logger.debug("")
@@ -17,8 +18,6 @@ class FavoriteStockViewModel(QObject):
         self.qmlContext.setContextProperty("favoriteStockViewModel", self)
 
         self._stockList = []
-        self.priceInfoKeys = ['시가', '고가', '저가', '현재가', '기준가', '대비기호', '전일대비', '등락율', '거래량', '거래대비']
-        self.priceInfoKeys_ = ['시가', '고가', '저가', '현재가', '기준가', '전일대비기호', '전일대비', '등락율', '거래량', '전일거래량대비']
 
         Client.getInstance().registerRealDataCallback("stock_price_real", self.__onStockPriceReal)
 
@@ -39,22 +38,13 @@ class FavoriteStockViewModel(QObject):
     def load(self):
         rows = DbHelper.getInstance().selectTableFavorite()
         stockList = []
-        stockPriceList = []
         logger.debug(rows)
         for item in rows:
             stockList.append({'name': item[0], 'code': item[1]})
 
         logger.debug(stockList)
 
-        result = Client.getInstance().stocks_info([x["code"] for x in stockList], "1003")
-        logger.debug(f"result:{result}")
-        for info in result:
-            priceInfo = {key: info[key] for key in self.priceInfoKeys_ if key in info}
-            priceItemData = StockPriceItemData(info['종목명'], info['종목코드'], priceInfo, fromSingleInfo=False)
-            logger.debug(priceItemData)
-            stockPriceList.append(priceItemData)
-
-        self.stockList = stockPriceList
+        self.__updateStockList([x["code"] for x in stockList])
 
     @pyqtSlot(str, str)
     def add(self, name: str, code: str):
@@ -64,27 +54,17 @@ class FavoriteStockViewModel(QObject):
 
         DbHelper.getInstance().insertStockToTableFavorite(name, code)
 
-        result = Client.getInstance().stock_basic_info(code, "1003")
-        if len(result) > 0:
-            priceInfo = {key: result[0][key] for key in self.priceInfoKeys if key in result[0]}
-            priceItemData = StockPriceItemData(name, code, priceInfo)
-            self._stockList.append(priceItemData)
-
-        logger.debug(self._stockList)
-        self.stockListChanged.emit()
+        self.__updateStockList([x.code for x in self._stockList] + [code])
 
     @pyqtSlot(str)
     def delete(self, code: str):
-        stockToRemove = None
         for stock in self._stockList:
             if stock.code == code:
                 DbHelper.getInstance().deleteStockFromTableFavorite(code)
-                stockToRemove = stock
                 break
 
-        self._stockList.remove(stockToRemove)
-        logger.debug(self._stockList)
-        self.stockListChanged.emit()
+        codeList = [x.code for x in self._stockList]
+        self.__updateStockList([x for x in codeList if x not in [code]])
 
     @pyqtSlot(str, result=bool)
     def isFavoriteStock(self, code):
@@ -92,6 +72,18 @@ class FavoriteStockViewModel(QObject):
             if stock.code == code:
                 return True
         return False
+
+    def __updateStockList(self, codeList):
+        result = Client.getInstance().stocks_info(codeList, "1003")
+        logger.debug(f"result:{result}")
+        stockPriceList = []
+        for info in result:
+            priceInfo = {key: info[key] for key in self.priceInfoKeys_ if key in info}
+            priceItemData = StockPriceItemData(info['종목명'], info['종목코드'], priceInfo, fromSingleInfo=False)
+            logger.debug(priceItemData)
+            stockPriceList.append(priceItemData)
+
+        self.stockList = stockPriceList
 
     @pyqtSlot(tuple)
     def __onStockPriceReal(self, data):
